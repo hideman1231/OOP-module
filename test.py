59db6f3ab2f676e317cbf088c83ca2d48350ea56
Kirill <smakvlad@gmail.com>
2021-02-23 10:34:20 +0200

    tests

---
+++ test.py
@@ -0,0 +1,34 @@
+from unittest import TestCase
+from unittest.mock import patch, Mock, mock_open
+
+from models import Enemy, Player
+
+
+class TestEnemy(TestCase):
+
+    @patch('models.Enemy.select_attack', return_value=1)
+    def test_select_attack(self, select_attack):
+        self.assertEqual(select_attack(), 1)
+
+
+class TestPlayer(TestCase):
+
+    def setUp(self):
+        self.enemy = Enemy(1, 1)
+        self.player = Player('test', 1, 1, 1)
+        self.mock = Mock()
+
+    def test_fight(self):
+        self.assertEqual(self.player.fight(1, 1), 0)
+        self.assertEqual(self.player.fight(1, 3), -1)
+        self.assertEqual(self.player.fight(3, 1), 1)
+
+    def test_validate_names(self):
+        self.assertIsNone(self.player.validate_names(self.player.name))
+
+    def test_validate_names_mock(self):
+        with patch('builtins.open', mock_open(read_data='name')):
+            self.assertIsNone(self.player.validate_names(self.player.name))
+
+    def test_validate_allowed_attacks(self):
+        self.assertIsNone(self.player.validate_allowed_attacks())
